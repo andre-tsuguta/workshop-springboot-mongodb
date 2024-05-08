78821121c910a4bf00d7497cf70a8b45a499949a
Andre Tsuguta <andre.mat@gmail.com>
2024-05-08 17:18:41 -0300

    Obtendo um usuário por id

--- src/main/java/com/andretsuguta/workshopmongo/services/UserService.java
+++ src/main/java/com/andretsuguta/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andretsuguta.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andretsuguta.workshopmongo.domain.User;
 import com.andretsuguta.workshopmongo.repositories.UserRepository;
+import com.andretsuguta.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto Nao encontrado"));
 	}
 	
 }
