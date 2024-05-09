bdf1021ac830b52c13368fe8df5bf66670ad38ee
Andre Tsuguta <andre.mat@gmail.com>
2024-05-09 16:01:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/andretsuguta/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.andretsuguta.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andretsuguta.workshopmongo.domain.Post;
+import com.andretsuguta.workshopmongo.repositories.PostRepository;
+import com.andretsuguta.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto Nao encontrado"));
+	}
+
+}
