3db0ce38fffdf5600689d3a5f9c82e939d2f558c
Andre Tsuguta <andre.mat@gmail.com>
2024-05-09 16:33:19 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/andretsuguta/workshopmongo/domain/Post.java
+++ src/main/java/com/andretsuguta/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.andretsuguta.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.andretsuguta.workshopmongo.dto.AuthorDTO;
+import com.andretsuguta.workshopmongo.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comment = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,4 +101,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
