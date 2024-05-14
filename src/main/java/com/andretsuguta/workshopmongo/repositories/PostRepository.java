5412c399c628b326d862204ab627ad8cc6914fea
Andre Tsuguta <andre.mat@gmail.com>
2024-05-14 10:43:22 -0300

    Consulta simples com query methods

--- src/main/java/com/andretsuguta/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/andretsuguta/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.andretsuguta.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.andretsuguta.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	public List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/andretsuguta/workshopmongo/resources/PostResource.java
+++ src/main/java/com/andretsuguta/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.andretsuguta.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andretsuguta.workshopmongo.domain.Post;
@@ -24,4 +27,9 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
